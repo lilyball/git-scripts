18614698e81e40efb48fb18c418e2fd238be2d56
Lily Ballard <lily@sb.org>
2014-12-01 15:15:53 -0800

    Fix some issues with the color function in utils.bash

--- utils.bash
+++ utils.bash
@@ -27,9 +27,12 @@ die() {
 
 # Usage:
 #   fatal <msg>
+#
+# Prints the fatal message and returns (not exits) with 128. Callers
+# are expected to then return themselves.
 fatal() {
     printf 'fatal: %s\n' "${1-'expected parameter to `fatal`'}" >&2
-    exit 128
+    return 128
 }
 
 # === Color ===
@@ -50,7 +53,7 @@ set_color() {
         config=$1
         shift
     fi
-    (( $# > 0 )) || fatal "set_color: expected <color> parameter"
+    (( $# > 0 )) || { fatal "set_color: expected <color> parameter"; return; }
     git config --get-color "$config" "$1"
 }
 
@@ -78,6 +81,10 @@ set_color() {
 # This command will only print color if the stdout is a terminal, unless either
 # `-f` is passed, which forces color, or `-b <boolconfig>` is passed, which tests
 # `git config --get-colorbool`.
+#
+# Note: the output of `git config --get-colorbool` is cached between calls.
+utils_color_cache_key=()
+utils_color_cache_value=()
 color() {
     local newline=yes color=
     [[ -t 1 ]] && color=yes
@@ -88,28 +95,40 @@ color() {
             -f)
                 color=yes;;
             -b)
-                (( $# > 1 )) || fatal "color: expected parameter to flag $1"
-                git config --get-colorbool "$2"
-                if (( $? == 0 )); then
-                    color=yes
-                else
-                    color=
+                (( $# > 1 )) || { fatal "color: expected parameter to flag $1"; return; }
+                shift
+                color=auto
+                # check our cache
+                local idx
+                for idx in "${!utils_color_cache_key[@]}"; do
+                    if [[ ${utils_color_cache_key[$idx]} == "$1" ]]; then
+                        color=${utils_color_cache_value[$idx]}
+                        break
+                    fi
+                done
+                if [[ $color == auto ]]; then
+                    git config --get-colorbool "$1"
+                    if (( $? == 0 )); then
+                        color=yes
+                    else
+                        color=
+                    fi
+                    utils_color_cache_key+=("$2")
+                    utils_color_cache_value+=("$color")
                 fi;;
-            -*)
-                die 2 "unknown flag $1";;
             *)
                 break;;
         esac
         shift
     done
     while (( $# > 0 )); do
-        local config=()
+        local config=
         if [[ $1 == -c ]]; then
-            (( $# > 1 )) || fatal "color: expected parameter to flag $1"
-            config=(-c "$2")
+            (( $# > 1 )) || { fatal "color: expected parameter to flag $1"; return; }
+            config=$2
             shift 2
         fi
-        [[ -n $color ]] && set_color "${config[@]}" "$1"
+        [[ -n $color ]] && set_color ${config:+"$config"} "$1"
         shift
         (( $# == 0 )) && break
         printf %s "$1"
